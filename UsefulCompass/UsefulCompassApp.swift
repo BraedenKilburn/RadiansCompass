e65e02ae6606d63dd15a214132197f7a72d81639
Braeden Kilburn <braedenk99@gmail.com>
2022-11-07 21:47:24 -0700

    Initial Commit

---
+++ UsefulCompass/UsefulCompassApp.swift
@@ -0,0 +1,17 @@
+//
+//  UsefulCompassApp.swift
+//  UsefulCompass
+//
+//  Created by Braeden Kilburn on 11/7/22.
+//
+
+import SwiftUI
+
+@main
+struct UsefulCompassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
